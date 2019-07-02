1db2033de6ccd19433a2ddf6165231b1d639df76
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-02 13:46:22 -0600

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+type Cats = {
+  name: string;
+  image: string;
+  stats?: { 
+    breed: string, 
+    weight: number, 
+    color: string 
+  };
+  kittenMittens: boolean;
+}
+
+interface IAppProps {
+  /* No props for this component */
+}
+
+interface IAppState {
+  cats: Array<Cats>
+}
+
+class App extends React.Component <{/* where props would go */}, IAppState> {
+
+  public static defaultProps = {
+    catMultiplier: 9
+  }
+
+  constructor(props: IAppProps) {
+    super(props);
+      this.state = {
+        cats: []
+      }
+  }
+
+  public addArguments(p: number, q: string): number {
+    return p + q;
+  }
+  render() {
+    return (
+      <div className="App">
+        <Cats />
     </div>
   );
+}
 }
 
 export default App;
